c46e3d95294f236602485c9ab7f6942f6b27c140
Adriribra11 <91127760+Adriribra11@users.noreply.github.com>
2022-03-10 09:28:14 +0100

    Add files via upload

--- BuscarARivero.py
+++ BuscarARivero.py
@@ -1,26 +1,13 @@
-#Al iniciar el programa se muestra el menú.
-#Seleccionamos la opción deseada. En mi caso la 2, buscar.
 #Pregunta a quien quieres buscar.
 #Si lo encuentra lo muestra. Si no dice que el contacto no existe y que lo agregemos desde el menú
 
-def agenda():
-    print ("#---------------------------------#")
-    print ("#              Menú               #")
-    print ("#---------------------------------#")
-    print ("#    1.-  Añadir/modificar        #")
-    print ("#    2.-  Buscar                  #")
-    print ("#    3.-  Borrar                  #")
-    print ("#    4.-  Listar                  #")
-    print ("#---------------------------------#")
-
-def buscar_ARivero():  
-    buscar=input("¿Qué contacto buscas?: ")
+def buscar_ARivero(nombre, agenda):  
+    #Te aparece un mensaje y pide que digas que contacto buscas.
+    buscar=input("¿Qué contacto buscas?: ") 
+    #Si existe el contacto aparecerá su información
     print (contactos[buscar])
     time.sleep(3)
     os.system("clear")
+    #Si no existe el contacto, aparecerá el siguiente mensaje.
     if buscar not in contactos:
        print("El contacto no existe, agregualo desde el menú")
-agenda()
-option =int(input("Elige la opción que quieras: "))
-while option == 2 :
-    buscar_ARivero()
